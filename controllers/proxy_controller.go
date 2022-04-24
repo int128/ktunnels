2568d1d1e181517822785073729ccb6afd154f8b
Hidetake Iwata <int128@gmail.com>
2022-04-24 09:47:56 +0900

    WIP

--- controllers/proxy_controller.go
+++ controllers/proxy_controller.go
@@ -18,6 +18,11 @@ package controllers
 
 import (
 	"context"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -39,18 +44,61 @@ type ProxyReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Proxy object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
 func (r *ProxyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	var proxy ktunnelsv1.Proxy
+	if err := r.Get(ctx, req.NamespacedName, &proxy); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
+	// TODO: add prefix to name
+	var proxyDeployment appsv1.Deployment
+	if err := r.Get(ctx, req.NamespacedName, &proxyDeployment); err != nil {
+		if err := client.IgnoreNotFound(err); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	proxyDeploymentSpec := appsv1.DeploymentSpec{
+		Replicas: pointer.Int32Ptr(1),
+		Template: corev1.PodTemplateSpec{
+			Spec: corev1.PodSpec{
+				Containers: []corev1.Container{{
+					Name:  "envoy",
+					Image: "envoyproxy/envoy:v1.23-latest",
+					Args:  []string{"--config-yaml", ``},
+					Resources: corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{
+							corev1.ResourceCPU:    resource.MustParse("10m"),
+							corev1.ResourceMemory: resource.MustParse("10Mi"),
+						},
+					},
+				}},
+			},
+		},
+	}
+
+	if proxyDeployment.Name == "" {
+		proxyDeployment = appsv1.Deployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: req.Namespace,
+				Name:      req.Name,
+			},
+			Spec: proxyDeploymentSpec,
+		}
+		if err := r.Create(ctx, &proxyDeployment); err != nil {
+			log.Error(err, "unable to create a proxy deployment")
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{}, nil
+	}
+
+	proxyDeployment.Spec = proxyDeploymentSpec
+	if err := r.Update(ctx, &proxyDeployment); err != nil {
+		log.Error(err, "unable to update the proxy deployment")
+		return ctrl.Result{}, err
+	}
 	return ctrl.Result{}, nil
 }
 
