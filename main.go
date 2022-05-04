fe7aa36cb4e592519edbdfbebd3c940fcef43ec4
Hidetake Iwata <int128@gmail.com>
2022-05-05 08:40:22 +0900

    Scaffold APIs and controllers (#10)
    * kubebuilder create api --group ktunnels --version v1 --kind Proxy
    
    * Uncomment
    
    * kubebuilder create api --group ktunnels --version v1 --kind Tunnel
    
    * Fix lint

--- main.go
+++ main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	ktunnelsv1 "github.com/int128/ktunnels/api/v1"
+	"github.com/int128/ktunnels/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(ktunnelsv1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +78,20 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.ProxyReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Proxy")
+		os.Exit(1)
+	}
+	if err = (&controllers.TunnelReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Tunnel")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
