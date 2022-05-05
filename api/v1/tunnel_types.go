bcc41bfc8b7df6b3484cbc212b8ce0312146f8a8
Hidetake Iwata <int128@gmail.com>
2022-05-05 09:46:42 +0900

    Add tunnel controller

--- api/v1/tunnel_types.go
+++ api/v1/tunnel_types.go
@@ -20,16 +20,21 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // TunnelSpec defines the desired state of Tunnel
 type TunnelSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Destination hostname of this tunnel.
+	Host string `json:"host,omitempty"`
 
-	// Foo is an example field of Tunnel. Edit tunnel_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Destination port of this tunnel.
+	Port int32 `json:"port,omitempty"`
+
+	// Name of proxy resource to register.
+	ProxyNameRef string `json:"proxyNameRef,omitempty"`
+
+	// Transit port of the proxy.
+	// This value is automatically set by proxy controller. Do not set this manually.
+	// +optional
+	TransitPort *int32 `json:"transitPort,omitempty"`
 }
 
 // TunnelStatus defines the observed state of Tunnel
