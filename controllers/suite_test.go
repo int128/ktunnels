43f21ee22580453930234de01b87d94520e758a3
Hidetake Iwata <int128@gmail.com>
2022-09-25 18:17:33 +0900

    Apply patch of kubebuilder v3.6.0 (#46)
    Co-authored-by: update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -19,12 +19,13 @@ package controllers
 import (
 	"context"
 	"path/filepath"
-	ctrl "sigs.k8s.io/controller-runtime"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
 	"k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
