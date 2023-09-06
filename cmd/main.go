1b30ced3f36331b2c70a9f8a6c40dc9f3b3ee64a
update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 07:44:18 +0000

    Upgrade from kubebuilder v3.11.1 to v3.12.0

--- cmd/main.go
+++ cmd/main.go
@@ -30,6 +30,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	ktunnelsv1 "github.com/int128/ktunnels/api/v1"
 	"github.com/int128/ktunnels/internal/controller"
@@ -67,8 +68,7 @@ func main() {
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
+		Metrics:                metricsserver.Options{BindAddress: metricsAddr},
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "a39e7441.int128.github.io",
