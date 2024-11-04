09d1cebb529c135644cb6770f486f5b67e1ab33b
Hidetake Iwata <int128@gmail.com>
2024-11-04 18:50:58 +0900

    Fix suite_test.go

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -52,8 +52,6 @@ var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 	ctx, cancel := context.WithCancel(context.TODO())
 
-	ctx, cancel = context.WithCancel(context.TODO())
-
 	By("bootstrapping test environment")
 	testEnv := &envtest.Environment{
 		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
